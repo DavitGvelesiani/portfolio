8dcd0f0e56abb6771fa1466222f5a375ceeaf7cc
DavitGvelesiani <98581644+DavitGvelesiani@users.noreply.github.com>
2022-12-12 14:36:57 +0400

    updated data.js

--- src/data/data.js
+++ src/data/data.js
@@ -4,6 +4,7 @@ import FoodOrder from '../assets/projects/food-order.png';
 import Meetups from '../assets/projects/meetups.png';
 import Simon from '../assets/projects/simon.png';
 import DrumKit from '../assets/projects/drum.jpg';
+import Youtube from '../assets/projects/youtube.jpg';
 
 export const data=[
     {
@@ -21,28 +22,35 @@ export const data=[
         live:"https://chat-app-gvelesiani.netlify.app",
     },
     {
-        id:3,
+        id: 3,
+        name: "Youtube 2.0",
+        image: Youtube,
+        github: "https://github.com/DavitGvelesiani/youtube2.0",
+        live: "https://youtube-alt.netlify.app"
+    },
+    {
+        id:4,
         name:"Food-order app",
         image:FoodOrder,
         github:"https://github.com/DavitGvelesiani/Food-Order-App",
         live:"https://davitgvelesiani.github.io/Food-Order-App/",
     },
     {
-        id:4,
+        id:5,
         name:"React-meetups",
         image:Meetups,
         github:"https://github.com/DavitGvelesiani/React-meetups",
         live:"https://react-meetups-lyart.vercel.app/",
     },
     {
-        id:5,
+        id:6,
         name:"Simon Game",
         image:Simon,
         github:"https://github.com/DavitGvelesiani/Simon-Game",
         live:"https://davitgvelesiani.github.io/Simon-Game/",
     },
     {
-        id:6,
+        id:7,
         name:"Drum Kit",
         image:DrumKit,
         github:"https://github.com/DavitGvelesiani/Drum-Kit",
